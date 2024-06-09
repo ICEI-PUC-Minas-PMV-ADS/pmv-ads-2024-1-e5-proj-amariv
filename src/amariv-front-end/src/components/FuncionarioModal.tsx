f5bf0ce09402eb59a6f24ef03dca9fc66852fc68
Eduardo Lobo <103695641+EdduLobo@users.noreply.github.com>
2024-06-09 18:08:19 -0300

    regexUpdate

--- src/amariv-front-end/src/components/FuncionarioModal.tsx
+++ src/amariv-front-end/src/components/FuncionarioModal.tsx
@@ -26,51 +26,31 @@ const FuncionarioModal: React.FC<FuncionarioModalProps> = ({
   onSave,
   onCancel,
 }) => {
-  const [errors, setErrors] = useState<Partial<FuncionarioInfo>>({});
+  const [errors, setErrors] = useState<{ [key: string]: string }>({});
 
   const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFuncionarioInfo({ ...funcionarioInfo, suportaPeso: event.target.checked });
   };
 
-  const validateForm = () => {
-    const newErrors: Partial<FuncionarioInfo> = {};
-
-    if (!funcionarioInfo.nome.trim()) {
-      newErrors.nome = 'Nome é obrigatório';
+  const validate = () => {
+    let tempErrors: { [key: string]: string } = {};
+    if (!funcionarioInfo.nome) tempErrors.nome = 'Nome é obrigatório';
+    if (!funcionarioInfo.email) tempErrors.email = 'Email é obrigatório';
+    else if (!/\S+@\S+\.\S+/.test(funcionarioInfo.email)) tempErrors.email = 'Email inválido';
+    if (!funcionarioInfo.sexo) tempErrors.sexo = 'Sexo é obrigatório';
+    if (!funcionarioInfo.telefone) tempErrors.telefone = 'Telefone é obrigatório';
+    if (!funcionarioInfo.cargo) tempErrors.cargo = 'Cargo é obrigatório';
+    if (!funcionarioInfo.senha) tempErrors.senha = 'Senha é obrigatória';
+    else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(funcionarioInfo.senha)) {
+      tempErrors.senha = 'A senha deve ter no mínimo 8 caracteres, incluindo uma letra maiúscula, uma letra minúscula, um número e um caractere especial';
     }
-
-    if (!funcionarioInfo.email.trim()) {
-      newErrors.email = 'Email é obrigatório';
-    } else if (!/\S+@\S+\.\S+/.test(funcionarioInfo.email)) {
-      newErrors.email = 'Email inválido';
-    }
-
-    if (!funcionarioInfo.sexo) {
-      newErrors.sexo = 'Sexo é obrigatório';
-    }
-
-    if (!funcionarioInfo.telefone.trim()) {
-      newErrors.telefone = 'Telefone é obrigatório';
-    }
-
-    if (!funcionarioInfo.cargo) {
-      newErrors.cargo = 'Cargo é obrigatório';
-    }
-
-    if (!funcionarioInfo.senha.trim()) {
-      newErrors.senha = 'Senha é obrigatória';
-    } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(funcionarioInfo.senha)) {
-      newErrors.senha = 'Senha deve ter pelo menos um caractere, uma letra maiúscula, uma letra minúscula e um número';
-    }
-
-    setErrors(newErrors);
-    return Object.keys(newErrors).length === 0;
+    setErrors(tempErrors);
+    return Object.keys(tempErrors).length === 0;
   };
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-
-    if (validateForm()) {
+    if (validate()) {
       onSave(event);
     }
   };
@@ -86,9 +66,9 @@ const FuncionarioModal: React.FC<FuncionarioModalProps> = ({
             id="nome"
             value={funcionarioInfo.nome}
             onChange={(e) => setFuncionarioInfo({ ...funcionarioInfo, nome: e.target.value })}
-            className={`w-full py-2 px-3 rounded border border-gray-300 focus:outline-none focus:border-blue-500 ${errors.nome ? 'border-red-500' : ''}`}
+            className="w-full py-2 px-3 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
           />
-          {errors.nome && <div className="text-red-500 text-sm mt-1">{errors.nome}</div>}
+          {errors.nome && <span className="text-red-500 text-sm">{errors.nome}</span>}
         </div>
         <div className="mb-3">
           <label htmlFor="email" className="text-sm text-gray-600">Email:</label>
@@ -97,9 +77,9 @@ const FuncionarioModal: React.FC<FuncionarioModalProps> = ({
             id="email"
             value={funcionarioInfo.email}
             onChange={(e) => setFuncionarioInfo({ ...funcionarioInfo, email: e.target.value })}
-            className={`w-full py-2 px-3 rounded border border-gray-300 focus:outline-none focus:border-blue-500 ${errors.email ? 'border-red-500' : ''}`}
+            className="w-full py-2 px-3 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
           />
-          {errors.email && <div className="text-red-500 text-sm mt-1">{errors.email}</div>}
+          {errors.email && <span className="text-red-500 text-sm">{errors.email}</span>}
         </div>
         <div className="mb-3">
           <label htmlFor="sexo" className="text-sm text-gray-600">Sexo:</label>
@@ -107,13 +87,13 @@ const FuncionarioModal: React.FC<FuncionarioModalProps> = ({
             id="sexo"
             value={funcionarioInfo.sexo}
             onChange={(e) => setFuncionarioInfo({ ...funcionarioInfo, sexo: e.target.value })}
-            className={`w-full py-2 px-3 rounded border border-gray-300 focus:outline-none focus:border-blue-500 ${errors.sexo ? 'border-red-500' : ''}`}
+            className="w-full py-2 px-3 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
           >
             <option value="">Selecione</option>
             <option value="masculino">Masculino</option>
             <option value="feminino">Feminino</option>
           </select>
-          {errors.sexo && <div className="text-red-500 text-sm mt-1">{errors.sexo}</div>}
+          {errors.sexo && <span className="text-red-500 text-sm">{errors.sexo}</span>}
         </div>
         <div className="mb-3">
           <label htmlFor="telefone" className="text-sm text-gray-600">Telefone:</label>
@@ -122,9 +102,9 @@ const FuncionarioModal: React.FC<FuncionarioModalProps> = ({
             id="telefone"
             value={funcionarioInfo.telefone}
             onChange={(e) => setFuncionarioInfo({ ...funcionarioInfo, telefone: e.target.value })}
-            className={`w-full py-2 px-3 rounded border border-gray-300 focus:outline-none focus:border-blue-500 ${errors.telefone ? 'border-red-500' : ''}`}
+            className="w-full py-2 px-3 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
           />
-          {errors.telefone && <div className="text-red-500 text-sm mt-1">{errors.telefone}</div>}
+          {errors.telefone && <span className="text-red-500 text-sm">{errors.telefone}</span>}
         </div>
         <div className="mb-3">
           <label htmlFor="cargo" className="text-sm text-gray-600">Cargo:</label>
@@ -132,14 +112,14 @@ const FuncionarioModal: React.FC<FuncionarioModalProps> = ({
             id="cargo"
             value={funcionarioInfo.cargo}
             onChange={(e) => setFuncionarioInfo({ ...funcionarioInfo, cargo: e.target.value })}
-            className={`w-full py-2 px-3 rounded border border-gray-300 focus:outline-none focus:border-blue-500 ${errors.cargo ? 'border-red-500' : ''}`}
+            className="w-full py-2 px-3 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
           >
-            <option value="Todos">Selecione</option>
+            <option value="">Selecione</option>
             <option value="Motorista">Motorista</option>
             <option value="Reciclagem">Reciclagem</option>
             <option value="outro">Outro</option>
           </select>
-          {errors.cargo && <div className="text-red-500 text-sm mt-1">{errors.cargo}</div>}
+          {errors.cargo && <span className="text-red-500 text-sm">{errors.cargo}</span>}
         </div>
         <div className="mb-3">
           <label htmlFor="senha" className="text-sm text-gray-600">Senha:</label>
@@ -148,9 +128,9 @@ const FuncionarioModal: React.FC<FuncionarioModalProps> = ({
             id="senha"
             value={funcionarioInfo.senha}
             onChange={(e) => setFuncionarioInfo({ ...funcionarioInfo, senha: e.target.value })}
-            className={`w-full py-2 px-3 rounded border border-gray-300 focus:outline-none focus:border-blue-500 ${errors.senha ? 'border-red-500' : ''}`}
+            className="w-full py-2 px-3 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
           />
-          {errors.senha && <div className="text-red-500 text-sm mt-1">{errors.senha}</div>}
+          {errors.senha && <span className="text-red-500 text-sm">{errors.senha}</span>}
         </div>
         <div className="mb-3">
           <label htmlFor="suportaPeso" className="text-sm text-gray-600 mr-2">Suporta Peso:</label>
@@ -161,7 +141,6 @@ const FuncionarioModal: React.FC<FuncionarioModalProps> = ({
             onChange={handleCheckboxChange}
             className="mr-2"
           />
-          <span className="text-sm text-gray-600"></span>
         </div>
         <div className="flex justify-end mt-4">
           <button
