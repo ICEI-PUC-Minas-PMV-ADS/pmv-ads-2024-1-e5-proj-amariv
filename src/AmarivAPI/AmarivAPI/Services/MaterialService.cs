61d77e4f3a3699466ae3e33a8a02c42e4e10b6dd
João Lucas <joaolucaspinheirotorres@gmail.com>
2024-06-11 22:28:57 -0300

    Update

--- src/AmarivAPI/AmarivAPI/Services/MaterialService.cs
+++ src/AmarivAPI/AmarivAPI/Services/MaterialService.cs
@@ -70,25 +70,30 @@ namespace AmarivAPI.Services
 
         public Result DeletaMaterial(int id_material)
         {
+            if(id_material == 1 || id_material == 2 || id_material == 3 || id_material == 4)
+            {
+                return Result.Fail("Não e possivel excluir os materiais basicos!");
+            }
             try
             {
                 Material material = _context.Materiais.FirstOrDefault(m => m.Id == id_material);
 
                 if (material != null)
                 {
-                    _context.Remove(material);
+                    material.Delete = true;
+                    _context.Update(material);
                     _context.SaveChanges();
                     return Result.Ok();
                 }
                 else
                 {
-                    return Result.Fail("Material não encontrado para exclusão!");
+                    return Result.Fail("Não foi possível encontrar o Material!!!");
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
-                return Result.Fail("Erro ao excluir material!");
+                return Result.Fail("Erro ao excluir o material cadastrado.");
             }
         }
 
@@ -96,7 +101,7 @@ namespace AmarivAPI.Services
         {
             try
             {
-                List<Material> materiais = _context.Materiais.ToList();
+                List<Material> materiais = _context.Materiais.Where(x => x.Delete == false).ToList();
 
                 if (materiais.Count == 0)
                 {
