b835ba7a482305b57c724f5a97fc21d0100620a9
vagpinto <vagpinto@sga.pucminas.br>
2024-06-23 09:01:37 -0300

    Ajuste

--- src/amariv-front-end/src/hooks/useApi.ts
+++ src/amariv-front-end/src/hooks/useApi.ts
@@ -4,7 +4,6 @@ import { TOKEN_KEY } from '../Constants'
 export const useApi = axios.create(
   {
     baseURL: "https://api.amariv.com",
-    //baseURL: "http://localhost:5100",
     headers: {
       'Authorization': `Bearer ${window.localStorage.getItem(TOKEN_KEY)}`,
       "Content-type": "application/json; chatset=utf-8",
